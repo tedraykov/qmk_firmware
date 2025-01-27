925462c971850756ecacb6b835a0deadebc6c23b
Teodor Raykov <tedraykov@gmail.com>
2025-01-27 13:22:20 +0200

    add remapping for layer 1 and 2

---
+++ keyboards/bastardkb/scylla/keymaps/tedraykov/config.h
@@ -0,0 +1,2 @@
+#define TAPPING_TERM 150
+#define QUICK_TAP_TERM 0
--- keyboards/bastardkb/scylla/keymaps/tedraykov/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/tedraykov/keymap.c
@@ -16,8 +16,6 @@
  */
 
 #include QMK_KEYBOARD_H
-#define TAPPING_TERM 150
-#define QUICK_TAP_TERM 0
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -25,30 +23,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              //-------------------------------------------------//-----------------------------------------------------------//
                              KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_LSFT, KC_A, KC_S, KC_D, MT(MOD_LGUI, KC_F), KC_G, KC_H, MT(MOD_RGUI, KC_J), KC_K, KC_L, KC_SCLN, KC_QUOT,
+                             KC_LSFT, KC_A, KC_S, MT(MOD_LALT, KC_D), MT(MOD_LGUI, KC_F), KC_G, KC_H, MT(MOD_RGUI, KC_J), MT(MOD_RALT, KC_K), KC_L, KC_SCLN, KC_QUOT,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_LCTL, KC_SPC, KC_LSFT, KC_RSFT, KC_BSPC, KC_ENT,
+                             KC_LSFT, LT(1, KC_SPC), KC_LCTL, KC_RSFT, LT(2, KC_BSPC), KC_ENT,
+                             //-------------------------------------------------//-----------------------------------------------------------//
                              _______, _______, _______, _______),
 
     [1] = LAYOUT_split_4x6_5(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
                              //---------------------------------------------------------//-----------------------------------------------------------//
-                             QK_BOOT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_P7, KC_P8, KC_P9, _______, KC_PLUS,
+                             QK_BOOT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_7, KC_8, KC_9, _______, KC_PLUS,
                              //---------------------------------------------------------//-----------------------------------------------------------//
-                             _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_LPRN, KC_RPRN, KC_P4, KC_P5, KC_P6, KC_MINS, KC_PIPE,
+                             _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_LPRN, KC_RPRN, KC_4, KC_5, KC_6, KC_MINS, KC_PIPE,
                              //---------------------------------------------------------//-----------------------------------------------------------//
-                             _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, KC_EQL, KC_UNDS,
+                             _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3, KC_EQL, KC_UNDS,
                              //---------------------------------------------------------//-----------------------------------------------------------//
-                             KC_LCTL, KC_HOME, KC_TRNS, KC_TRNS, KC_RALT, KC_RGUI, KC_SPC, KC_BSPC, KC_RCTL, KC_ENT),
+                             KC_LCTL, KC_HOME, KC_TRNS, KC_LSFT, KC_LCTL, KC_RGUI,
+                             //---------------------------------------------------------//-----------------------------------------------------------//
+                             KC_SPC, KC_BSPC, KC_RCTL, KC_ENT),
 
     [2] = LAYOUT_split_4x6_5(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
                              //---------------------------------------------------------//--------------------------------------------------------------//
-                             _______, _______, RM_PREV,  RM_TOGG, RM_NEXT, KC_LBRC, KC_RBRC, _______, KC_NUM, KC_INS, KC_SCRL, KC_MUTE,
+                             _______, _______, RM_PREV,  KC_UP, RM_NEXT, KC_LBRC, KC_RBRC, _______, KC_NUM, KC_INS, KC_SCRL, KC_MUTE,
                              //---------------------------------------------------------//--------------------------------------------------------------//
-                             _______, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_LPRN, KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
+                             _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_LPRN, KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
                              //---------------------------------------------------------//--------------------------------------------------------------//
                              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD,
                              //---------------------------------------------------------//--------------------------------------------------------------//
-                             KC_LCTL, KC_HOME, KC_TRNS, KC_TRNS, KC_RALT, QK_BOOT, KC_SPC, KC_BSPC, KC_RCTL, KC_ENT),
+                             KC_LCTL, KC_HOME, KC_TRNS, KC_TRNS, KC_RALT, QK_BOOT,
+                             //---------------------------------------------------------//--------------------------------------------------------------//
+                             KC_SPC, KC_BSPC, KC_RCTL, KC_ENT),
 };
